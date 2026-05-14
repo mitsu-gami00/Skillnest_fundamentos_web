72c9defb7a4fbd736e19877f6d7a889ac89cd50c
mitsu-gami00 <ignacioorellana@liceovvh.cl>
2026-05-14 13:01:30 -0400

    ok

--- STACK_JS/01_introduccion_js/static/js/script.js
+++ STACK_JS/01_introduccion_js/static/js/script.js
@@ -1,3 +1,7 @@
 function cambiarTexto() {
+    let titulo = "Hola, bienvenido"
+    if (document.getElementById("titulo").innerText == titulo)
            document.getElementById("titulo").innerText = "Has cambiado el texto con JS";
-       }
+       else
+         document.getElementById("titulo").innerText = "Hola, bienvenido";
+    }
