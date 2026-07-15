76ffabe259e7179e1d05a455edbec21869cdbd34
mitsu-gami00 <ignacioorellana@liceovvh.cl>
2026-07-15 09:53:18 -0400

    ok

---
+++ STACK_JS/07_arreglos/static/js/script.js
@@ -0,0 +1,9 @@
+let nombre1 = "Ana";
+let nombre2 = "Pedro";
+let nombre3 = "María";
+let nombre4 = "Carlos"
+
+function X() {
+let nombres = ["Ana", "Pedro", "Maria", "Carlos"];
+alert(`${nombres[2]}`)
+}
